ad1aeaa2405fb753e42db499764ae47cf59c7697
Denis Fedosevich <fedosgrodno@gmail.com>
2021-04-24 21:42:37 +0300

    feat: implemented a reaction to a click  todo items

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import '../scss/app.scss';
 
-/* Your JS Code goes here */
 const todoList = document.querySelector('.todo-task__list');
 const todoItems = todoList.childNodes;
 const formAddTask = document.forms.addTask;
@@ -32,7 +31,6 @@ function createTodoItem(text) {
   return item;
 }
 
-
 function formAddTaskSubmitHandler(evt) {
   evt.preventDefault();
 
@@ -46,4 +44,34 @@ function formAddTaskSubmitHandler(evt) {
   updateTodoItems();
 }
 
+function todoListClickHandler(evt) {
+  const task = evt.target.closest('.todo-task__item');
+
+  if (task) {
+    const taskContent = task.querySelector('.todo-task__content');
+
+    if (evt.target.className === 'button button--important' || evt.target.className === 'button button--unimportant') {
+      const buttonImportant = task.querySelector('.button');
+
+      buttonImportant.classList.toggle('button--important');
+      buttonImportant.classList.toggle('button--unimportant');
+      taskContent.classList.toggle('todo-task__content--important');
+
+      if (buttonImportant.textContent === 'Mark important') {
+        buttonImportant.textContent = 'Not important';
+      } else {
+        buttonImportant.textContent = 'Mark important';
+      }
+    } else if (evt.target.className === 'button button-remove') {
+      task.remove();
+    } else {
+      taskContent.classList.toggle('todo-task__content--completed');
+    }
+
+    updateTodoItems();
+  }
+}
+
 formAddTask.addEventListener('submit', formAddTaskSubmitHandler);
+todoList.addEventListener('click', todoListClickHandler);
+
